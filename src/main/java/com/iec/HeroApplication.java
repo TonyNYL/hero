866277619fbff97cc3010cf23b3f8751dc946956
TonyNYL <ningyuanlong@gmail.com>
2017-05-17 13:33:52 +0800

    欢迎来到王者荣耀

---
+++ src/main/java/com/iec/HeroApplication.java
@@ -0,0 +1,22 @@
+package com.iec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class HeroApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home(){
+		return "欢迎来到王者荣耀！！！";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroApplication.class, args);
+	}
+}
